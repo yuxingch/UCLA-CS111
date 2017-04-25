c8cbbe637494e3562dc0d055c24782d2f7661af3
Yuxing Chen <yxchen2828@gmail.com>
2017-04-25 10:21:40 -0700

    Update lab1b-server.c

--- Project 1B/lab1b-server.c
+++ Project 1B/lab1b-server.c
@@ -4,6 +4,7 @@
 //  Copyright © 2017 Yuxing Chen. All rights reserved.
 //
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -19,18 +20,16 @@
 
 #include <netdb.h>
 #include <netinet/in.h>
-#include <mcrypt.h>
+#include <mcrypt.h> //  a data encryption library
 #include <string.h>
 
 #define PORT    'p'
 #define ENCRYPT 'e'
 #define BUFSIZE 2048
 
-
-int is_encrypt = 0;
 int keyfd;
+int is_encrypt = 0;
 MCRYPT encryptfd, decryptfd;
-
 
 pid_t pid = -1;
 
@@ -47,46 +46,48 @@ int pipe_from_child[2];
 //  (one describing the keyboard (stdin) and one describing the pipe that returns output from the shell.)
 struct pollfd pfd[2];
 
-//  before exit, restore the saved terminal mode
-void prepare_to_exit (void) {
-    if (is_encrypt) {
-        mcrypt_generic_deinit(encryptfd);
-        mcrypt_module_close(encryptfd);
-        mcrypt_generic_deinit(decryptfd);
-        mcrypt_module_close(decryptfd);
-    }
-    
+void print_msg() {
     int status = 0;
     //  get the shell's exit status
     waitpid(0, &status, 0);
     fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\n", WTERMSIG(status), WEXITSTATUS(status));
 }
 
+//  before exit, restore the saved terminal mode
+void prepare_to_exit (int withmsg) {
+    if (is_encrypt) {   /*  deinit encryption/decryption    */
+        mcrypt_generic_deinit(encryptfd);
+        mcrypt_module_close(encryptfd);
+        mcrypt_generic_deinit(decryptfd);
+        mcrypt_module_close(decryptfd);
+    }
+    if (withmsg) {  /*  print the EXIT message  */
+        print_msg();
+    }
+}
+
+//  SIGNAL handler
 void my_handler(int signum) {
-    /*  Deal with ^C in --shell mode
-     once received SIGINT, the child process had already been interrupted
-     we need to restore the terminal mode and exit
-     according to piazza, we regard this as normal execution, with exit code 0   */
+    /*  Deal with ^C, exit code 0   */
     if (signum == SIGINT) {
-        prepare_to_exit();
+        prepare_to_exit(1);
         exit(0);
     }
     /*  Deal with kill xxxxx    */
     if (signum == SIGPIPE) {
-        prepare_to_exit();
+        prepare_to_exit(1);
         exit(0);
     }
     
+    /*  Deal with the client exits  */
     if (signum == SIGTERM) {
-        prepare_to_exit();
+        prepare_to_exit(1);
         exit(0);
     }
 }
 
 int main(int argc, char *argv[]) {
-    
-    
-    //  Extend and refactor your program to support a --shell argument
+    //  Extend and refactor your program to support --port and --encrypt
     static struct option long_options[] = {
         {"port",    required_argument,  0,  PORT },
         {"encrypt", required_argument,  0,  ENCRYPT},
@@ -96,9 +97,10 @@ int main(int argc, char *argv[]) {
     int temp = 0;
     int long_index = 0;
     int n_port = 0;
-    int keysize;
+    int keysize =16;
+    int cursize;
     char *key;
-    char *IV1, *IV2;
+    char *IV1;
     
     while ( (temp = getopt_long(argc, argv, "", long_options, &long_index)) != -1) {
         switch(temp){
@@ -108,16 +110,23 @@ int main(int argc, char *argv[]) {
                 signal(SIGPIPE, my_handler);
                 signal(SIGTERM, my_handler);
                 break;
-            case ENCRYPT:
+            case ENCRYPT:   //  --encrypt
                 keyfd = open(optarg, O_RDONLY);
+                //  grab the key from the specified file
                 struct stat st;
-                fstat(keyfd, &st);
-                keysize = (int) st.st_size;
-                key = (char*) malloc(keysize * sizeof(char));
-                read(keyfd, key, keysize);
+                if (fstat(keyfd, &st) < 0) {
+                    close(keyfd);
+                    fprintf(stderr, "Error: fail to return information about the specified key file.\n");
+                    exit(1);
+                }
+                cursize = (int) st.st_size;
+                key = (char*) malloc(cursize * sizeof(char));
+                read(keyfd, key, cursize);
                 close(keyfd);
-                /*  initialization  */
-                /*  encryption module   */
+                /*  Initialization
+                 *  We need to modules: one deals with encryption, another for decryption
+                 */
+                //  encryption module
                 encryptfd = mcrypt_module_open("twofish", NULL, "cfb", NULL);
                 if (encryptfd == MCRYPT_FAILED) {
                     fprintf(stderr, "Error: Fail to open a module.\n");
@@ -131,29 +140,26 @@ int main(int argc, char *argv[]) {
                     fprintf(stderr, "Error: Fail to initialize encrypt.\n");
                     exit(1);
                 }
-                /*  decryption module   */
+                //  decryption module
                 decryptfd = mcrypt_module_open("twofish", NULL, "cfb", NULL);
                 if (decryptfd == MCRYPT_FAILED) {
                     fprintf(stderr, "Error: Fail to open a module.\n");
                     exit(1);
                 }
-//                IV2 = malloc(mcrypt_enc_get_iv_size(decryptfd));
-//                for (int i=0; i< mcrypt_enc_get_iv_size(decryptfd); i++) {
-//                    IV2[i]=rand();
-//                }
                 if (mcrypt_generic_init(decryptfd, key, keysize, IV1) < 0) {
                     fprintf(stderr, "Error: Fail to initialize decrypt.\n");
                     exit(1);
                 }
-                is_encrypt = 1;
+                is_encrypt = 1; //  Set the encryption flag
                 break;
             default:    /*  invalid argument    */
-                fprintf(stderr, "Usage: ./lab1b-server --port=# [--encrypt=file]");
+                fprintf(stderr, "Usage: ./lab1b-server --port=# [--encrypt=file]\n");
                 exit(1);
                 break;
         }
     }
     
+    /*  Socket Part */
     int sockfd, newsockfd, clilen;
     struct sockaddr_in serv_addr, cli_addr;
     
@@ -162,13 +168,13 @@ int main(int argc, char *argv[]) {
     
     if (sockfd < 0) {
         perror("ERROR opening socket");
-        prepare_to_exit();
+        prepare_to_exit(0);
+        close(sockfd);
         exit(1);
     }
     
     /* Initialize socket structure */
     memset((char *) &serv_addr, 0, sizeof(serv_addr));
-//    portno = 5001;
     
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -177,7 +183,8 @@ int main(int argc, char *argv[]) {
     /* Now bind the host address using bind() call.*/
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
-        prepare_to_exit();
+        prepare_to_exit(0);
+        close(sockfd);
         exit(1);
     }
     
@@ -194,8 +201,10 @@ int main(int argc, char *argv[]) {
     //	b/c multiple connections
     
     if (newsockfd < 0) {
-        perror("ERROR on accept");
-        prepare_to_exit();
+        perror("ERROR on accept.\n");
+        prepare_to_exit(0);
+        close(newsockfd);
+        close(sockfd);
         exit(1);
     }
     
@@ -204,12 +213,16 @@ int main(int argc, char *argv[]) {
     //  pipes for communication
     if (pipe(pipe_to_child) == -1) {
         fprintf(stderr, "pipe() failed.\n");
-        prepare_to_exit();
+        prepare_to_exit(0);
+        close(newsockfd);
+        close(sockfd);
         exit(1);
     }
     if (pipe(pipe_from_child) == -1) {
         fprintf(stderr, "pipe() failed.\n");
-        prepare_to_exit();
+        prepare_to_exit(0);
+        close(newsockfd);
+        close(sockfd);
         exit(1);
     }
     
@@ -220,8 +233,8 @@ int main(int argc, char *argv[]) {
         close(pipe_from_child[1]);
         
         int rv;
-        pfd[0].fd = newsockfd;
-        pfd[0].events = POLLIN; // check for normal data
+        pfd[0].fd = newsockfd;  //  we should read from the socket
+        pfd[0].events = POLLIN; //  check for normal data
         
         pfd[1].fd = pipe_from_child[0];
         pfd[1].events = POLLIN; // check for normal data
@@ -230,7 +243,9 @@ int main(int argc, char *argv[]) {
             rv = poll(pfd, 2, 0);
             if (rv == -1) {
                 fprintf(stderr, "error in poll().\n");
-                prepare_to_exit();
+                prepare_to_exit(1);
+                close(newsockfd);
+                close(sockfd);
                 exit(1);
             }
             if (pfd[0].revents & POLLIN) {
@@ -254,12 +269,12 @@ int main(int argc, char *argv[]) {
                         //  <cr> or <lf> should go to shell as <lf>
                         write(pipe_to_child[1], lf, 1);
                     } else if (cur == 0x04) {           /*  ^D/EOF  */
-                        //  upon receiving an EOF (^D) from the terminal, close the pipe to the shell
+                        //  upon receiving an EOF (^D) from the socket, close the pipe to the shell
                         close(pipe_to_child[1]);
                     } else if (cur == 0x03) {           /* ^C   */
                         //  When your program reads a ^C from the keyboard
                         //  use kill(2) to send a SIGINT to the shell process
-                        kill(0, SIGINT);   //  sigint
+                        kill(pid, SIGINT);
                     } else {                            /* normal characters */
                         write(pipe_to_child[1], buf1+k, 1);
                     }
@@ -280,20 +295,15 @@ int main(int argc, char *argv[]) {
                         mcrypt_generic (encryptfd, buf2+k, 1);
                     }
                     write(newsockfd, buf2+k, 1);
-//                    char cur = buf2[k];
-//                    if (cur == 0x0D || cur == 0x0A) {
-//                        //  <cr> or <lf> should echo as <cr><lf>
-//                        write(newsockfd, crlf, 2);
-//                    } else {    /* normal characters */
-//                        write(newsockfd, buf2+k, 1);
-//                    }
                 }
             }
             //  upon receiving EOF or polling-error from the shell, we know that we have all of the output the shell has generated
             if (pfd[1].revents & (POLLHUP+POLLERR)) {
                 close(pipe_from_child[0]);  //  close another end of the pipe
-                //  ... print out exit status ?
-                prepare_to_exit();
+                //  print out exit status and exit
+                prepare_to_exit(1);
+                close(newsockfd);
+                close(sockfd);
                 exit(0);
             }
         }
@@ -318,15 +328,22 @@ int main(int argc, char *argv[]) {
         execvp_argv[1] = NULL;
         if (execvp("/bin/bash", execvp_argv) == -1) {
             fprintf(stderr, "execvp() failed.\n");
-            prepare_to_exit();
+            prepare_to_exit(1);
+            close(newsockfd);
+            close(sockfd);
             exit(1);
         }
     } else {
         fprintf(stderr, "fork() failed.\n");
-        prepare_to_exit();
+        prepare_to_exit(1);
+        close(newsockfd);
+        close(sockfd);
         exit(1);
     }
+    
+    //  close all fds, print out message and exit
     close(newsockfd);
-    prepare_to_exit();
+    close(sockfd);
+    prepare_to_exit(1);
     exit(0);
 }
